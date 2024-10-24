ffa510258003168fe3a73f04f15fdf237c5932fd
zedutra <zedutra93@gmail.com>
2024-10-24 15:18:59 -0300

    Ajustes

---
+++ script.js
@@ -0,0 +1,13 @@
+// Funcoes de modal
+function openModal(element) {
+    var modal = document.getElementById("imageModal");
+    var modalImg = document.getElementById("modalImage");
+    
+    modal.style.display = "flex";
+    modalImg.src = element.src;
+}
+
+function closeModal() {
+    var modal = document.getElementById("imageModal");
+    modal.style.display = "none";
+}
